59388b387bab29e9be1673bb9ec4c8c7a598464f
sreeharsha <harsha@indavest.com>
2014-12-21 10:38:53 +0530

    MVC First Class

---
+++ MvcDemo1/MvcDemo1/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDemo1.Models;
+
+namespace MvcDemo1.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+        private static User users = new User();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Display Users";
+            ViewBag.users = users.userModels;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserModels model)
+        {
+            users.AddUser(model);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcDemo1/MvcDemo1/Models/UserModels.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcDemo1.Models
+{
+    public class UserModels
+    {
+        [Required]
+        public string Id { get; set; }
+        
+        [Display(Name = "EmailAddress")]
+        [Required(ErrorMessage = "Missed Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DOB { get; set; }
+        
+        [Required]
+        [Range(10000, 100000)]
+        public Decimal Salary { get; set; }
+    }
+
+    public class User
+    {
+        public User()
+        {
+            userModels.Add(new UserModels { 
+                Id = "1",
+                Email = "harsha@soandso.com",
+                DOB = DateTime.Today,
+                Salary = 10000
+            });
+            userModels.Add(new UserModels
+            {
+                Id = "2",
+                Email = "harsha1@soandso.com",
+                DOB = DateTime.Today,
+                Salary = 10000
+            });
+            userModels.Add(new UserModels
+            {
+                Id = "3",
+                Email = "harsha2@soandso.com",
+                DOB = DateTime.Today,
+                Salary = 10000
+            });
+        }
+
+        public IList<UserModels> userModels = new List<UserModels>();
+
+        public void AddUser(UserModels user)
+        {
+            userModels.Add(user);
+        }
+    }
+}
